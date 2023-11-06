4897441a205758db2e8dfe3d6587b630e545b97c
bmisra03 <71111367+bmisra03@users.noreply.github.com>
2023-11-06 17:12:40 -0500

    Task 5 works

--- Project2DataFiles/task_5.m
+++ Project2DataFiles/task_5.m
@@ -1,16 +1,28 @@
+%Load fundamental matrix
+load('F.mat')
+
+%Load pixel values
+load("ImagePointLocations.mat");
+
 totalDistance = 0;
 
-for i = 1:length(x1)
-    % homogeneous coordinates
-    point1 = [x1(i); y1(i); 1];
-    point2 = [x2(i); y2(i); 1]; 
+for i = 1:size(image1Locations,2)
+    % convert point to homogeneous coordinates
+    point1 = [image1Locations(:,i);1];
+    point2 = [image2Locations(:,i);1]; 
+    
+    % compute epipolar lines
+    l2 = F*point1;
+    l1 = F'*point2;
 
     % calculate square geometric distance
-    distance1 = ((epiLines1(1)*x1(i) + epiLines1(2)*y1(i) + epiLines1(3))^2) / (epiLines1(1)^2 + epiLines1(2)^2);
-    distance2 = ((epiLines2(1)*x2(i) + epiLines2(2)*y2(i) + epiLines2(3))^2) / (epiLines2(1)^2 + epiLines2(2)^2);
-   
-    symmEpiDist = distance1 + distance2; 
-    totalDistance = totalDistance + symmEpiDist;
+    dist1 = dot(l1, point1)^2/(l1(1)^2+l1(2)^2);
+    dist2 = dot(l2, point2)^2/(l2(1)^2+l2(2)^2);
+
+    totalDistance = totalDistance + dist1 + dist2;
 end
 
-meanSymmetricEpipolarDistance = totalDistance / length(x1)
+% to get mean distance, we get the total distance and divide by 
+% the total number of points (2*39=78 points in this case)
+meanDistance = totalDistance/(2*size(image1Locations, 2));
+disp(meanDistance);
